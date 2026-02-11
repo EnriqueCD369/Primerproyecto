e3c2ad0f446b657608c469c70500fcf7316cbd74
EnriqueCD369 <chandiazenrique@gmail.com>
2026-02-10 18:34:25 -0600

    Se agrego los palametros de conexión a la base de datos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\Pagina;
-use Illuminate\Support\Facades\App;
+use App\Models\Pagina;
+use Iluminate\Illuminate\Support\Facades\App;
 
 class HomeController extends Controller
 {
-    public function empresa()
+    public function __invoke()
     {
-    $datos["nombre"]="Jesús Enrique Chan Díaz";
-    $datos["fecha"]="2026-02-03";  
-    $datos["actividad"]="Desarrollo de software";
-    $datos["Descripcion_about"]="Empresa de dicada al desarrollo de software a medida de sus clientes";
-    $datos["texto_ejemplo"]="Aqui va la descripción del texto de ejemplo";
+        return view('hello');
+    }
 
-    $usuarios=new Pagina();
-    $datos["listausuarios"]=$usuarios->ObtenerListado();
-    return view('empresa', $datos);
+    public function empresa(){
+        $datos["nombre"]="Jose Eduardo Ac Garcia";
+        $datos["fecha"]="2026-12-15";
+        $datos["actividad"]="Desarrollo de software";
+        $datos["descripcion_about"]="Empresa dedicada al desarrollo de software a la medida de sus clientes.";
+        $datos["texto_ejemplo"]="Aqui va la descripcion del texto de ejemplo";
+        //$usuarios=new Pagina();
+        //$datos["listadousuarios"]=$usuarios->ObtenerListado();
+        return view('empresa',$datos);
     }
 }
--- resources/views/empresa.blade.php
+++ resources/views/empresa.blade.php
@@ -1,68 +1,44 @@
-@extends('layout.app')
-@section('titulopagina','Empresa E-commerce')
+@extends('layouts.app')
+ 
+@section('titulopagina', 'Empresa E-Commerce')
+ 
 @push('css')
-<style>
-    .fondo{
-        background: #302886;
-}
-
-.img-responsive{
-    width: 100%;
-    height: 100%;
-}
-
-</style>
-
+    <style>
+        .fondo {
+            background: #302886;
+        }
+ 
+        .img-responsive {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 @endpush
-
+ 
 @section('titulo')
-Bienvenido a la página de EC
+    Bienvenido a la página de EC
 @endsection
-
-@section("Subtitulo");
-Explorando las oportunidades con Laravel 12
+ 
+@section('subtitulo')
+    Explorando las oportunidades con Laravel 12
 @endsection
-
-@section("link1","Active");
-@section("titulo1")
+ 
+@section( "link1", 'Active')
+@section( "titulo1")
 <h1>About Me</h1>
 @endsection
 @section("descripcion_about")
-    {{$descripcion_about}}
+{{ $descripcion_about }}
 @endsection
 @section("Autor")
-    {{$nombre}}
+{{ $nombre }}
 @endsection
-@section("acctividad", $actividad)
+@section("actividad",$actividad)
 @section("texto_ejemplo")
-    {{$texto_ejemplo}}
+  {{ $texto_ejemplo }}
 @endsection
+ 
 @section("contenido_listado")
-    <h2>Listado de Usuarios Registados</h2>
- <u1>
-    @if(@isset($listadousuarios))
-        <table id="tablausuarios" class="table table-striped table bordered">
-            <thead>
-                <tr>
-                    <h1>Nombre</h1>
-                    <h1>Email</h1>
-                    <h1>Telefono</h1>
-                    <h1>Calle</h1>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($listadousuarios as $Usuario)
-             <tr>
-                <td>{{$usuario->name}}</td>
-                <td>{{$usuario->email}}</td>
-                <td>{{$usuario->telefono}}</td>
-                <td>{{$usuario->calle}}</td>    
-             </tr>
-            @endforeach
-            </tbody>
-        </table>
-        @else
-        <p> La variable de listado de usuarios no etsa definida</p>
-    @endif
- </u1>
- @endsection
+  <h2>Listado de Usuarios Registrados</h2>
+ 
+@endsection
